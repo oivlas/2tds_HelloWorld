a4b577045db13b637ccc32d7245d5540c7c29792
Oivlas <71229362+oivlas@users.noreply.github.com>
2020-09-24 11:34:30 -0300

    Mudança no  contador novamente... agora ficou bom
    Vouala!...

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
    public static void main (String []args){
-      for(int i=1;i<=17;i++){
+      for(int i=1;i<=3;i++){
          System.out.println("Olá equipe 2TDSF da FIAP "+i);
       }
    }
