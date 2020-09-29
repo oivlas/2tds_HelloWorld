2bc7de60c8ee1ed49d2eca6206f249498a3ad7c2
Oivlas <71229362+oivlas@users.noreply.github.com>
2020-09-29 08:22:33 -0300

    Atualização para ajustes da turma 2TDSA
    Feito para realizar testes no Jenkins.

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
    public static void main (String []args){
-      for(int i=1;i<=3;i++){
-         System.out.println("Olá equipe 2TDSF da FIAP "+i);
+      for(int i=1;i<=10;i++){
+         System.out.println("Olá equipe 2TDSA da FIAP "+i);
       }
    }
 }
