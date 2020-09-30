8d24304ee2f2daaaa1bdb99a129d25fe34c9d451
Oivlas <71229362+oivlas@users.noreply.github.com>
2020-09-30 08:33:22 -0300

    Atualização msg texto e quantidade de execução
    Atualização da mensagem de texto e quantidade de execução

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
    public static void main (String []args){
-      for(int i=1;i<=11;i++){
-         System.out.println("Olá equipe 2TDSJ da FIAP "+i);
+      for(int i=1;i<=5;i++){
+         System.out.println("Olá equipe 2TDSJ da FIAP. Estamos na versão 2.0 "+i);
       }
    }
 }
