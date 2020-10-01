b81880b37161e83f05e406159bb594e44c03e3de
Oivlas <71229362+oivlas@users.noreply.github.com>
2020-10-01 08:31:52 -0300

    Atualização do contatdor e do texto da msg
    Atualização do contatdor e do texto da msg

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
    public static void main (String []args){
-      for(int i=1;i<=5;i++){
-         System.out.println("Olá equipe 2TDSJ da FIAP. Estamos na versão 2.0 "+i);
+      for(int i=1;i<=18;i++){
+         System.out.println("Olá equipe 2TDSF da FIAP. Estamos na versão 2.0 "+i);
       }
    }
 }
